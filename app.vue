caf13a7a3c756ca5331ad2ecd34ac4de5931936e
Nuno Góis <github@nunogois.com>
2023-10-09 15:27:09 +0100

    initial commit

---
+++ app.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { useFlag, useFlagsStatus } from '@unleash/proxy-client-vue'
+
+const { flagsReady } = useFlagsStatus()
+
+const nuxtApp = useNuxtApp()
+const unleashClient = nuxtApp.$unleashClient
+console.info(`Unleash client is fetching from ${unleashClient.url}`)
+
+const enabled = useFlag('vue-test-feature')
+</script>
+
+<template>
+  <div v-if="!flagsReady">Loading...</div>
+  <div v-else>
+    <span v-if="enabled"> Vue test feature is enabled </span>
+    <span v-else> Vue test feature is disabled </span>
+  </div>
+</template>
---
+++ plugins/unleash.ts
@@ -0,0 +1,34 @@
+import {
+  UnleashClient,
+  plugin as unleashPlugin
+} from '@unleash/proxy-client-vue'
+
+const config = {
+  url: 'https://app.unleash-hosted.com/demo/api/frontend',
+  clientKey:
+    'proxy-client-vue:development.28d2246bf472159c395befee79623275882816bb9ff3e65c74f48e3c',
+  refreshInterval: 2,
+  appName: 'proxy-client-vue'
+}
+
+let unleashClient: UnleashClient | undefined
+
+export default defineNuxtPlugin(nuxtApp => {
+  if (!unleashClient) {
+    unleashClient = new UnleashClient(config)
+    nuxtApp.vueApp.use(unleashPlugin, { unleashClient })
+  }
+
+  if (process.server) {
+    console.info(
+      'Here are all the toggles we know of:',
+      unleashClient.getAllToggles()
+    )
+  }
+
+  return {
+    provide: {
+      unleashClient
+    }
+  }
+})
